52a30712cac3b4c87948ab689cf0c2ebdf3eb925
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-15 07:20:16 +0530

    Test Classes Added

---
+++ Junit Testing Java/src/operations/MarksCheck.java
@@ -0,0 +1,14 @@
+package operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarksCheck {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
