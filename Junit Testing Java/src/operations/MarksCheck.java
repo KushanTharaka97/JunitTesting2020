9805faa5175668f21dd600461f2c4169d57d8ef9
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-15 07:23:59 +0530

    TestClass

--- Junit Testing Java/src/operations/MarksCheck.java
+++ Junit Testing Java/src/operations/MarksCheck.java
@@ -2,13 +2,19 @@ package operations;
 
 import static org.junit.Assert.*;
 
+
+
 import org.junit.Test;
 
 public class MarksCheck {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ResultsReturn rs = new ResultsReturn();
+		String value = rs.getResultRank(80);
+		
+		assertEquals("A+", value);
+		
 	}
 
 }
